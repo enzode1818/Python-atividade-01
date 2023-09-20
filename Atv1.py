5337e076b2d3bc01cc738f2987b6c2107b7e80c2
enzode1818 <144919736+enzode1818@users.noreply.github.com>
2023-09-20 12:17:31 -0300

    Add files via upload

---
+++ Atv1.py
@@ -0,0 +1,16 @@
+print("Ola mundo")
+print("soma de dois numeros")
+print("mensagem de saudaçao")
+#numeros
+num1 = float(input("escreva um numero: "))
+num2 = float(input("escreva outro numero: "))
+#operações
+soma = num1 + num2
+subtracao = num1 - num2 
+multiplicacao = num1 * num2
+divisao = num1 / num2
+#exibicao
+print(f"soma: {soma}")
+print(f"subtraçao: {subtracao}")
+print (f"multiplicaçao: {multiplicacao}")
+print(f"divisao: {divisao}")
